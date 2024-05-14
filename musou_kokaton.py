f81cfdf2c4c1504a4033af18f407bfc57c68ea61
riki <c0b2309670@edu.teu.ac.jp>
2024-05-14 15:12:36 +0900

    高速化

--- musou_kokaton.py
+++ musou_kokaton.py
@@ -88,6 +88,9 @@ class Bird(pg.sprite.Sprite):
         引数2 screen：画面Surface
         """
         sum_mv = [0, 0]
+        self.speed = 10
+        if key_lst[pg.K_LSHIFT]:
+            self.speed = 20
         for k, mv in __class__.delta.items():
             if key_lst[k]:
                 sum_mv[0] += mv[0]
